f81e02ab7d7e1fd7ee1cbf23be21e158be6c3dad
bryan lesht <bryan.lesht@colorado.edu>
2020-01-29 22:45:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly_with_third_tier
     railroads:
     {pieces:4,
     rent_in_dollars:
-    {one_piece_owned:"one",
+    {one_piece_owned:25,
     two_piece_owned:"two",
     three_piece_owned:"three",
     four_piece_owned:"four"
