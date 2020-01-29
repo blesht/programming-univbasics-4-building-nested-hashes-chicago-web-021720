7ee9d0a4049ccba1a1adb2b9eb52888e72adcedf
bryan lesht <bryan.lesht@colorado.edu>
2020-01-29 22:27:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,5 @@
 def base_hash
-  monopoly = {hey:"first"}
+  monopoly = {railroads:Hash.new}
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	monopoly 
